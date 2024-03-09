77ee94f41c4531e83b7112d417262e2c32b5d36e
Zayozin <zayocomercial@gmail.com>
2024-03-09 16:16:46 -0300

    finish challenge 003

--- Module D/challenge03/script.js
+++ Module D/challenge03/script.js
@@ -5,25 +5,27 @@ function calcular() {
     var delta = (b * b) - (4 * a * c); 
     var res = document.getElementById('res');
     var res2 = document.getElementById('res2');
-
-    res.innerHTML = `O valor de delta é: ${delta}`;
     
     if (a == 0) {
-        window.alert('[ERRO!] O valor de "a" não pode ser igual a 0')
+        window.alert('[ERRO!] O valor de "a" não pode ser igual a 0.')
+        res.innerHTML = `[ERRO!] Insira novos valores e tente novamente.`;
     }
     else if (delta < 0) {
-        res2.innerHTML = `Não há soluções reais na equação pois: delta < 0`;
+        res.innerHTML = `O valor de delta é: <strong>${delta}</strong>.`;
+        res2.innerHTML = `Não há soluções reais na equação pois: <strong>delta < 0</strong>.`;
         
     }
     else if (delta == 0) {
         var x1 = (-b + delta**0.5) / (2 * a);
-        res2.innerHTML = `O resultado é: ${x1}`;
+        res.innerHTML = `O valor de delta é: <strong>${delta}</strong>.`;
+        res2.innerHTML = `O resultado é: <strong>${x1}</strong>.`;
         
     }
     else if (delta > 0) {
         var x1 = (-b + delta**0.5) / (2 * a);
         var x2 = (-b - delta**0.5) / (2 * a);
-        res2.innerHTML = `Os resultados são: ${x1} e ${x2}`;
+        res.innerHTML = `O valor de delta é: <strong>${delta}</strong>.`;
+        res2.innerHTML = `Os resultados são: <strong>${x1}</strong> e <strong>${x2}</strong>.`;
     }
 }
 
