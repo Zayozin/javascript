61c9ecadd67e09aca48404dd8edf12b8fcb2a08a
Zayozin <zayocomercial@gmail.com>
2024-03-13 16:53:39 -0300

    -

--- Module E/challenge02/script.js
+++ Module E/challenge02/script.js
@@ -8,16 +8,20 @@ function começar() {
 
     if (isNaN(nini) || isNaN(nfim) || isNaN(npasso) || nini === '' || nfim === '' || npasso === '') {
         window.alert('[ERRO] Insira novos dados e tente novamente.');
-    } else if (nini >= nfim) {
-        window.alert('[ERRO] O valor inicial deve ser menor que o valor final.');
     } else if (npasso <= 0) {
         window.alert('[ERRO] O passo deve ser maior que zero.');
+    } else if (nini >= nfim) {
+        while (nini >= nfim) {
+            res.innerHTML += `${nini} &#x27A1; `;
+            nini -= npasso; // Abreviação para nini = nini + npasso;
+        }
+        res.innerHTML += '&#x1F3C1;';
+
     } else {
         while (nini <= nfim) {
             res.innerHTML += `${nini} &#x27A1; `;
             nini += npasso; // Abreviação para nini = nini + npasso;
         }
-    
         res.innerHTML += '&#x1F3C1;';
     }
 }
